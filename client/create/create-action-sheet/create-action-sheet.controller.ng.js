5d00d2a91d94cfb1e607074674a6d71b47fb2d70
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2015-11-01 14:06:24 -0500

    added bottom sheet for creating list & item

---
+++ client/create/create-action-sheet/create-action-sheet.controller.ng.js
@@ -0,0 +1,29 @@
+angular.module('fridgeListApp').controller('CreateActionSheetCtrl', CreateActionSheetCtrl);
+
+function CreateActionSheetCtrl($mdBottomSheet) {
+  var vm = this;
+
+  vm.select = select;
+  vm.items = [
+    {name: 'New List', icon: 'assignment', action: openCreateListModal},
+    {name: 'New Item', icon: 'add_box', action: openCreateItemModal},
+    {name: 'Scan Item', icon: 'photo_camera', action: openScanItemModal}
+  ];
+
+  function select(index) {
+    vm.items[index].action();
+    $mdBottomSheet.hide();
+  }
+
+  function openCreateListModal() {
+
+  }
+
+  function openCreateItemModal() {
+
+  }
+
+  function openScanItemModal() {
+
+  }
+}
--- client/main/main.controller.ng.js
+++ client/main/main.controller.ng.js
@@ -1,5 +1,15 @@
 angular.module('fridgeListApp').controller('MainCtrl', MainCtrl);
 
-function MainCtrl($scope, $mdSidenav) {
+function MainCtrl($scope, $mdBottomSheet) {
   var vm = this;
+
+  vm.openCreateSheet = openCreateSheet;
+
+  function openCreateSheet() {
+    $mdBottomSheet.show({
+      templateUrl: 'client/create/create-action-sheet/create-action-sheet.ng.html',
+      controller: 'CreateActionSheetCtrl',
+      controllerAs: 'action'
+    });
+  }
 }
