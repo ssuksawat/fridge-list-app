cb05a4a17d8a8c9f2c3bf801f4680c89fb5e417c
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2015-11-22 18:07:33 -0500

    show current location when screensize medium - small

--- client/components/top-toolbar/top-toolbar.directive.js
+++ client/components/top-toolbar/top-toolbar.directive.js
@@ -9,11 +9,41 @@ function TopToolbar() {
     controllerAs: 'toolbar'
   };
 
-  function TopToolbarCtrl($mdSidenav) {
+  function TopToolbarCtrl($rootScope, $state, $stateParams, $mdSidenav) {
     var vm = this;
 
-    vm.toggleMenu = function() {
-      $mdSidenav('sideMenu').toggle();
-    };
+    init();
+
+    vm.toggleMenu = () => $mdSidenav('sideMenu').toggle();
+
+    /* ----- EVENT LISTENERS ----- */
+
+    $rootScope.$on('$stateChangeSuccess', setCurrentLocation);
+
+    /* ----- PRIVATE ----- */
+
+    function init() {
+      vm.currentLocation = getLocation();
+    }
+
+    function getLocation() {
+      switch($state.current.name) {
+        case 'main.inventories':
+          return 'My Inventories';
+        case 'main.inventoryItems':
+          var currentList = Inventories.findOne($stateParams.listId);
+          return currentList.title;
+        case 'settings':
+          return 'Settings';
+        default:
+          return '';
+      }
+    }
+
+    function setCurrentLocation() {
+      vm.currentLocation = getLocation();
+      console.log('currentLocation: ', vm.currentLocation);
+    }
+
   }
 }
