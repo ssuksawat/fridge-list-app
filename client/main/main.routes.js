553a19516fbe877dd91e85627560da271dfe1331
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2015-11-22 18:06:55 -0500

    move 'inventories' subscription to main route resolve

--- client/main/main.routes.js
+++ client/main/main.routes.js
@@ -1,15 +1,26 @@
-angular.module('fridgeListApp')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('main', {
-        abstact: true,
-        templateUrl: 'client/main/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main',
-        resolve: {
-          "currentUser": ["$meteor", function($meteor){
-            return $meteor.requireUser();
-          }]
-        }
-      });
-  });
+angular
+  .module('fridgeListApp')
+  .config(MainRoutes);
+
+function MainRoutes($stateProvider) {
+  $stateProvider
+    .state('main', {
+      abstact: true,
+      templateUrl: 'client/main/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main',
+      resolve: {
+        'currentUser': ['$meteor', resolveCurrentUser],
+        'inventories': ['$meteor', resolveInventories]
+      }
+    });
+}
+
+function resolveCurrentUser($meteor) {
+  return $meteor.requireUser();
+}
+
+function resolveInventories($meteor) {
+  return $meteor.subscribe('inventories');
+
+}
